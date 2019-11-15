b5f9a836afc5c642f33ca0d4f80478d566c1dfdf
Nicklas Lanman <nicklasllanman@gmail.com>
2019-11-14 20:54:02 -0600

    Added Spinner to Profile

--- FrontEnd/src/pages/Profile.js
+++ FrontEnd/src/pages/Profile.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Row, Tooltip, Spinner } from 'reactstrap';
-// import { Link } from 'react-router-dom';
+import { Container, Row, Tooltip, Spinner, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import SectionHeader from '../components/SectionHeader';
 
 const Profile = (props) => {
 
+    const [id] = useState((props.location.value.id === 'undefined') ? -1 : props.location.value.id)
     const [tooltipOpen, setTooltipOpen] = useState(false);
     const toggle = () => setTooltipOpen(!tooltipOpen);
     const [resource, setResource] = useState(
@@ -31,19 +32,19 @@ const Profile = (props) => {
 
     useEffect(() => {
         try {
-        axios.get('http://localhost:5000/api/resources/' + props.location.value.id)
+        axios.get('http://localhost:5000/api/resources/' + id)
           .then(res => {
             const resourceData = res.data;
             setResource( resourceData )
           })
-      } catch {}}, [])
+      } catch {}}, [id])
 
     const Education = () => {
         let Education = []
         let width = resource.Education.length > 2 ? 4 : 6
         for (let i=0; i<resource.Education.length; i++) {
             Education = [...Education, 
-            <div className={"col-md-"+ width}>
+            <div key={Education.length} className={"col-md-"+ width}>
                 <p><b>School/University:</b>&nbsp;{resource.Education[i].School}</p>
                 <p><b>Location:</b>&nbsp;{resource.Education[i].Location}</p>
                 <p><b>Graduation Date:</b>&nbsp;{resource.Education[i].GradDate}</p>
@@ -68,7 +69,7 @@ const Profile = (props) => {
         let width = resource.Experience.length > 2 ? 4 : 6
         for (let i=0; i<resource.Experience.length; i++) {
             Experience = [...Experience, 
-            <div className={"col-md-"+ width}>
+            <div key={Experience.length} className={"col-md-"+ width}>
                 <p><b>Job Title:</b>&nbsp;{resource.Experience[i].JobTitle}</p>
                 <p><b>Job Company/Organization:</b>&nbsp;{resource.Experience[i].JobOrg}</p>
                 <p><b>Start Date:</b>&nbsp;{resource.Experience[i].JobStartDate}</p>
@@ -89,7 +90,7 @@ const Profile = (props) => {
         let width = resource.Project.length > 2 ? 4 : 6
         for (let i=0; i<resource.Project.length; i++) {
             Project = [...Project, 
-            <div className={"col-md-"+ width}>
+            <div key={Project.length} className={"col-md-"+ width}>
                 <p><b>Project Name:</b>&nbsp;{resource.Project[i].ProjName}</p>
                 <p><b>Project Association:</b>&nbsp;{resource.Project[i].ProjAssociation}</p>
                 <p><b>Project Date:</b>&nbsp;{resource.Project[i].ProjDate}</p>
@@ -105,7 +106,7 @@ const Profile = (props) => {
         let width = resource.Certification.length > 2 ? 4 : 6
         for (let i=0; i<resource.Certification.length; i++) {
             Certification = [...Certification, 
-            <div className={"col-md-"+ width}>
+            <div key={Certification.length} className={"col-md-"+ width}>
                 <p><b>Certification Name:</b>&nbsp;{resource.Certification[i].CertName}</p>
                 <p><b>Certification Association:</b>&nbsp;{resource.Certification[i].CertAssociation}</p>
                 <p><b>Certification Date:</b>&nbsp;{resource.Certification[i].CertDate}</p>
@@ -123,8 +124,6 @@ const Profile = (props) => {
 
     return (
         <div>
-            {console.log(resource)}
-            {console.log(resource.Id)}
             { resource.Id === null ? <div className="text-center"><Spinner color="primary" /></div> :
             <div>
                 <Header name={
@@ -197,6 +196,11 @@ const Profile = (props) => {
                     </Row>
                     <Row>
                         {Skills()}
+                    </Row>
+                    <Row>
+                        <Link className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 text-right" style={{ textDecoration: 'none'}} to="/resources">
+                            <Button style={{ }} color="primary">Return To Resources</Button>
+                        </Link>
                     </Row>
                 </Container> 
             </div>
