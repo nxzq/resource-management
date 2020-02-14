b271497e2502c40fb07f56d4b4632bceba9bd8ee
Nicklas Lanman <nicklasllanman@gmail.com>
2020-02-13 21:17:39 -0600

    added navigation hotkeys to helppage

--- FrontEnd/src/pages/Help.js
+++ FrontEnd/src/pages/Help.js
@@ -15,10 +15,10 @@ const Help = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                     <h2>Shortcuts:</h2>
-                    <p>Navigate to Overview Page: SHIFT + o</p> 
-                    <p>Navigate to Resource Page: SHIFT + r</p> 
-                    <p>Navigate to Help Page: SHIFT + h</p> 
-                    <p>Navigate to Settings Page: SHIFT + s</p> 
+                    <p>Navigate to Overview Page: &nbsp;&nbsp; SHIFT + o</p> 
+                    <p>Navigate to Resource Page: &nbsp;&nbsp; SHIFT + r</p> 
+                    <p>Navigate to Help Page: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SHIFT + h</p> 
+                    <p>Navigate to Settings Page: &nbsp;&nbsp;&nbsp; SHIFT + s</p> 
                 </div>
             </Container>
         </div>
